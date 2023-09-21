5dcbe612c179159fd9a282ee3e180e6f3b101a1a
gauravkharel <kharel619@gmail.com>
2023-09-21 19:39:46 +0545

    Implement css for subreddit detail page

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,9 +1,45 @@
-import { FC } from 'react'
+import MiniCreatePost from '@/components/MiniCreatePost'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
 
-interface pageProps {}
+interface PageProps {
+    params: {
+        slug: string
+    }
+}
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
+const page = async ({params}: PageProps) => {
+    const {slug} = params
+    const session = await getAuthSession()
+//    imp stuff to learn about t
+    const subreddit = await db.subreddit.findFirst({
+        where: {name: slug},
+        include: {
+            posts: {
+                include:{
+                    author: true,
+                    votes: true,
+                    comments: true,
+                    subreddit: true,
+                },
+                take: INFINITE_SCROLLING_PAGINATION_RESULTS
+            }
+        }
+    })
+
+    if(!subreddit) return notFound()
+
+  return (
+    <>
+        <h1 className='font-bold text-3xl md:text-4xl h-14'>
+            r/{subreddit.name}
+            <MiniCreatePost session={session} />
+            {/* TODO: Show post in the user feed */}
+        </h1>
+    </>
+  )    
 }
 
 export default page
