8d8ff773268531df69c9b95e4979899ab922d2f8
gauravkharel <kharel619@gmail.com>
2023-09-29 11:49:03 +0545

    Implemented comment section

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,3 +1,4 @@
+import CommentsSections from '@/components/CommentsSections'
 import EditorOutput from '@/components/EditorOutput'
 import PostVoteServer from '@/components/post-vote/PostVoteServer'
 import { buttonVariants } from '@/components/ui/Button'
@@ -65,7 +66,10 @@ const page= async ({params}: PageProps) => {
 
           <EditorOutput content={post?.content ?? cachedPost.content} />
 
-          
+          <Suspense fallback={<Loader2 className='h-5 w-5 animate-spin text-zinc-500' />}>
+            {/* @ts-expect-error server component */}
+            <CommentsSections postId={post?.id ?? cachedPost.id} />
+          </Suspense>
         </div>
       </div>
     </div>
